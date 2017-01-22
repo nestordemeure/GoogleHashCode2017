468eec283d32de6794f8190aaabc872b0597848b
nestordemeure <nestordemeure@gmail.com>
2017-01-22 11:57:36 +0100

    add Psed/priorityQueue basic files

---
+++ hashCode/GHC/GHC.fs
@@ -0,0 +1,18 @@
+module GHC
+
+open GHC.Domain
+open GHC.Import
+open GHC.Solve
+open GHC.Export
+
+//-------------------------------------------------------------------------------------------------
+
+
+
+//-------------------------------------------------------------------------------------------------
+// MAIN
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    0 // return an integer exit code
