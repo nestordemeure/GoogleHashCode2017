6adfe2ccc22056bcfcb806bbaa15f084ca538049
nestordemeure <nestordemeure@gmail.com>
2017-01-22 17:29:08 +0100

    added basic graph type

--- hashCode/GHC/Domain.fs
+++ hashCode/GHC/Domain.fs
@@ -1,12 +1,13 @@
 module GHC.Domain
 
 open FSharpx.Collections
+open System.Collections.Generic
 
 open GHC.Extensions
 
 //-------------------------------------------------------------------------------------------------
 
-
+//type graph = Dictionary<'key,'Node>
 
 //-------------------------------------------------------------------------------------------------
 
