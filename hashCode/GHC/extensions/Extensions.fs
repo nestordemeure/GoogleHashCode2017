b9aebf6f485f6a4cc76a96b6ba66ed792cf65012
nestor demeure <nestor demeure>
2017-01-24 13:16:49 +0100

    aded generic maxvalue to priorityqueue

--- hashCode/GHC/extensions/Extensions.fs
+++ hashCode/GHC/extensions/Extensions.fs
@@ -21,36 +21,3 @@ module Array =
       let temp = a.[i]
       a.[i] <- a.[j]
       a.[j] <- temp
-
-//-------------------------------------------------------------------------------------------------
-// GENERIC MAXVALUE (for generic priority queue)
-
-module LanguagePrimitives =
-  type MaxValue = MaxValue with
-      static member ($) (_:unit          , _:MaxValue) = ()
-      static member ($) (_:bool          , _:MaxValue) = true
-      static member ($) (_:char          , _:MaxValue) = Char.MaxValue
-      static member ($) (_:byte          , _:MaxValue) = Byte.MaxValue
-      static member ($) (_:sbyte         , _:MaxValue) = SByte.MaxValue
-      static member ($) (_:float         , _:MaxValue) = Double.MaxValue
-      static member ($) (_:int16         , _:MaxValue) = Int16.MaxValue
-      static member ($) (_:int           , _:MaxValue) = Int32.MaxValue
-      static member ($) (_:int64         , _:MaxValue) = Int64.MaxValue
-      static member ($) (_:float32       , _:MaxValue) = Single.MaxValue
-      static member ($) (_:uint16        , _:MaxValue) = UInt16.MaxValue
-      static member ($) (_:uint32        , _:MaxValue) = UInt32.MaxValue
-      static member ($) (_:uint64        , _:MaxValue) = UInt64.MaxValue
-      static member ($) (_:decimal       , _:MaxValue) = Decimal.MaxValue
-      static member ($) (_:DateTime      , _:MaxValue) = DateTime.MaxValue
-      static member ($) (_:DateTimeOffset, _:MaxValue) = DateTimeOffset.MaxValue
-      static member ($) (_:TimeSpan      , _:MaxValue) = TimeSpan.MaxValue
-
-  /// generic maxValue
-  let inline maxValue() :'r =  Unchecked.defaultof<'r> $ MaxValue
-
-  type MaxValue with
-      static member inline ($) ((_:'a*'b         ), _:MaxValue) = maxValue(), maxValue()
-      static member inline ($) ((_:'a*'b*'c      ), _:MaxValue) = maxValue(), maxValue(), maxValue()
-      static member inline ($) ((_:'a*'b*'c*'d   ), _:MaxValue) = maxValue(), maxValue(), maxValue(), maxValue()
-      static member inline ($) ((_:'a*'b*'c*'d*'e), _:MaxValue) = maxValue(), maxValue(), maxValue(), maxValue(), maxValue()
-
