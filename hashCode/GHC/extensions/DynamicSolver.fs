1a336918bd27b03af930089d9ee233dcff92dd46
nestor demeure <nestor demeure>
2017-01-29 00:23:51 +0100

    added a dynamic solution to the knapsack problem

--- hashCode/GHC/extensions/DynamicSolver.fs
+++ hashCode/GHC/extensions/DynamicSolver.fs
@@ -3,6 +3,8 @@ module GHC.Extensions.DynamicSolver
 open System
 open GHC.Extensions
 open GHC.Extensions.Common
+open System.Collections.Generic
+open ExtCore.Collections
 
 //-------------------------------------------------------------------------------------------------
 // SOLVER
@@ -32,6 +34,52 @@ let solve (initialState : 'State) (initialValue : 'Value) evaluateState developS
       search () // TODO : one could add a timeout here
   bestState, bestValue
 
+//-------------------------------------------------------------------------------------------------
+// KNAPSACK
 
+type Object = {weight : int ; price : int ; id : int}
 
+type State = {weight : int ; price : int ; objects : Object list}
 
+/// returns a list of object wich maximise the price while keeping sum(weight) <= maxWeight
+/// solution using dynamic programming
+/// might explode if maxWeight gets too big
+let knapsackDyn maxWeight (objects : Object list) =
+  let mutable solution = Array.create (maxWeight+1) {weight=0 ; price=0 ; objects=[]} 
+  let mutable previousSolution = {weight=0 ; price=0 ; objects=[]}
+  for ob in objects do 
+    previousSolution <- solution.[ob.weight-1]
+    for i = ob.weight to maxWeight do 
+       let newWeight = previousSolution.weight + ob.weight
+       let newPrice = previousSolution.price + ob.price
+       if (newPrice > solution.[i].price) && (newWeight <= i) then 
+          let newSolution = {weight = newWeight ; price = newPrice ; objects = ob :: previousSolution.objects}
+          previousSolution <- solution.[i]
+          solution.[i] <- newSolution
+       else previousSolution <- solution.[i]
+  solution.[maxWeight]
+(*
+/// objects : (weight*price) list
+let knapsackDij maxWeight objects =
+  let mutable bestState = {weight=0 ; price=0 ; objectNumber=0 ; objects=[] ; left=objects}
+  /// previously visited states (memoization)
+  let knownStates = Dictionary()
+  /// states and their current evaluations
+  let stateQueue = MaxMPriorityQueue.singleton 0 bestState
+  /// explores all possible states
+  let rec search () =
+    match MaxMPriorityQueue.popMax stateQueue with 
+    | None -> () 
+    | Some (price,state) when Dict.containsKey (state.weight,state.objectNumber) knownStates -> ()
+    | Some (price,state) -> 
+      for newState in develop maxWeight state state.left do
+        if not (MutableSet.contains newState knownStates) then 
+           MutableSet.add newState knownStates
+           if newState.price > bestState.price then 
+              bestState <- newState
+           MaxMPriorityQueue.push newState.price newState stateQueue
+      search () // TODO : one could add a timeout here
+  bestState
+*)
+// a state is a number of objects and a weight 
+// the state with the best price is kept 
