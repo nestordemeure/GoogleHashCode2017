603ddd3707fa1c1ec045e5d8fb678992eb700355
nestor demeure <nestor demeure>
2017-01-29 13:29:16 +0100

    worked on knapsack solver

--- hashCode/GHC/extensions/DynamicSolver.fs
+++ hashCode/GHC/extensions/DynamicSolver.fs
@@ -37,16 +37,17 @@ let solve (initialState : 'State) (initialValue : 'Value) evaluateState developS
 //-------------------------------------------------------------------------------------------------
 // KNAPSACK
 
+/// weight should be minimised and price maximised
 type Object = {weight : int ; price : int ; id : int}
-
 type State = {weight : int ; price : int ; objects : Object list}
+let emptyState = {weight=0 ; price=0 ; objects=[]} 
 
 /// returns a list of object wich maximise the price while keeping sum(weight) <= maxWeight
 /// solution using dynamic programming
 /// might explode if maxWeight gets too big
 let knapsackDyn maxWeight (objects : Object list) =
-  let mutable solution = Array.create (maxWeight+1) {weight=0 ; price=0 ; objects=[]} 
-  let mutable previousSolution = {weight=0 ; price=0 ; objects=[]}
+  let mutable previousSolution = emptyState
+  let solution = Array.create (maxWeight+1) emptyState 
   for ob in objects do 
     previousSolution <- solution.[ob.weight-1]
     for i = ob.weight to maxWeight do 
@@ -58,28 +59,38 @@ let knapsackDyn maxWeight (objects : Object list) =
           solution.[i] <- newSolution
        else previousSolution <- solution.[i]
   solution.[maxWeight]
+
+/// variation using graphs, uses only the useful weights (but not all of them unlike the classical dynamic version)
+/// similar because for each object, for each state, it tries to 1-add it to the state, 2-not add it
+/// a state represent the best combinaison for a given weight
+let knapsackDij maxWeight (objects : Object list) =
+  let mutable states = Map.singleton 0 emptyState
+  /// takes a state, add an object and update the states if it is an amelioration (better price for the same weight) + legal
+  let addObject (ob:Object) oldStates w st = 
+    let newSt = {weight=st.weight+ob.weight; price=st.price+ob.price; objects= ob :: st.objects}
+    if newSt.weight > maxWeight then oldStates else 
+      match Map.tryFind newSt.weight oldStates with 
+      | Some oldSt when oldSt.price >= newSt.price -> oldStates
+      | _ -> Map.add newSt.weight newSt oldStates
+  /// update the states by trying to add the object to all the states, one by one
+  for ob in objects do 
+    states <- Map.fold (addObject ob) states states
+  // maxBy price
+  Map.fold (fun acc _ st -> if st.price>acc.price then st else acc) emptyState states
 (*
-/// objects : (weight*price) list
-let knapsackDij maxWeight objects =
-  let mutable bestState = {weight=0 ; price=0 ; objectNumber=0 ; objects=[] ; left=objects}
-  /// previously visited states (memoization)
-  let knownStates = Dictionary()
-  /// states and their current evaluations
-  let stateQueue = MaxMPriorityQueue.singleton 0 bestState
-  /// explores all possible states
-  let rec search () =
-    match MaxMPriorityQueue.popMax stateQueue with 
-    | None -> () 
-    | Some (price,state) when Dict.containsKey (state.weight,state.objectNumber) knownStates -> ()
-    | Some (price,state) -> 
-      for newState in develop maxWeight state state.left do
-        if not (MutableSet.contains newState knownStates) then 
-           MutableSet.add newState knownStates
-           if newState.price > bestState.price then 
-              bestState <- newState
-           MaxMPriorityQueue.push newState.price newState stateQueue
-      search () // TODO : one could add a timeout here
-  bestState
+/// variation using graphs, uses only the useful weights (and not all of them like the dynamic version)
+let knapsackDij maxWeight (objects : Object list) =
+  let states = dict [0, emptyState]
+  /// takes a state, add an object and update the states if it is an amelioration (better price for the same weight) + legal
+  let addObject (ob:Object) w st = 
+    let newSt = {weight=st.weight+ob.weight; price=st.price+ob.price; objects= ob :: st.objects}
+    if newSt.weight <= maxWeight then 
+      match Dict.tryFind newSt.weight states with 
+      | Some oldSt when oldSt.price >= newSt.price -> ()
+      | _ -> states.[newSt.weight] <- newSt
+  /// add the object to all the states, one by one (possible conflict : add object should not add before the end of iter ?)
+  for ob in objects do Dict.iter (addObject ob) states
+  // maxBy price
+  Dict.fold (fun acc _ st -> if st.price>acc.price then st else acc) emptyState states
 *)
-// a state is a number of objects and a weight 
-// the state with the best price is kept 
+
