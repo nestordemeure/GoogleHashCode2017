9c72f93f0ec7a045a98b706d844b4cb8267ce2de
franck <franck.schmitt@hotmail.com>
2017-02-23 20:59:15 +0100

    ajout d'un tri des arrays de requetes

--- hashCode/GHC/cache.fs
+++ hashCode/GHC/cache.fs
@@ -51,3 +51,15 @@ let isSameVideo v1 v2 =
 let computeCache (videos, points, cacheNum, cacheSize) =
     let caches = fillCache cacheNum points
     fuseCache caches
+
+  
+
+let rec selectionneBests tailleMax result cache =
+    match cache with
+    | [] -> result
+    | p::q when p.poid > tailleMax -> selectionneBests tailleMax result q
+    | p::q -> selectionneBests (tailleMax-p.poid) (p::result) q
+
+let filterCaches caches tailleMax = 
+    Array.mapInPlace (selectionneBests tailleMax []) caches 
+    caches
