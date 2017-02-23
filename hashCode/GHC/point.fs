e4c643d1980e9ce787cd648f8f64e88a96371bef
nestordemeure <nestordemeure@gmail.com>
2017-02-23 19:40:16 +0100

    r

--- hashCode/GHC/point.fs
+++ hashCode/GHC/point.fs
@@ -12,8 +12,21 @@ let setPoids points videos = ()
 
 //-------------------------------------------------------------------------------------------------
 
+let collapseReqs vidId reqList =
+   List.reduce (fun acc r -> {acc with value = acc.value + r.value}) reqList
+
+let fuseReqs (point:Point) =
+   let newReqs = 
+      point.reqs
+      |> List.groupBy (fun r -> r.video )
+      |> List.map (fun (vidId,reqList) -> collapseReqs vidId reqList)
+   { point with reqs = newReqs }
+
+//-------------------------------------------------------------------------------------------------
+
 let computePoints (videos, points, cacheNum, cacheSize) = 
    // mettre les poids dans les requetes
    setPoids points videos
    // fusionner les requetes par video
+   Array.mapInPlace fuseReqs points
    // calculer un score pour chaque requette
