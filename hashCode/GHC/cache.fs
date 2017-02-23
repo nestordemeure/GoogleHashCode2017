fd3de4c85a1b00a4ffc265712ec4793a2a45a263
nestordemeure <nestordemeure@gmail.com>
2017-02-23 19:22:04 +0100

    fs

---
+++ hashCode/GHC/cache.fs
@@ -0,0 +1,7 @@
+module GHC.Cache
+
+open ExtCore.Collections
+
+open GHC.Extensions
+open GHC.Extensions.Common
+open GHC.Domain
---
+++ hashCode/GHC/point.fs
@@ -0,0 +1,7 @@
+module GHC.Point
+
+open ExtCore.Collections
+
+open GHC.Extensions
+open GHC.Extensions.Common
+open GHC.Domain
