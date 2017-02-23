e0cd32e88e9eed1487acbb9016412f6d11199ece
nestordemeure <nestordemeure@gmail.com>
2017-02-23 19:07:35 +0100

    p

--- hashCode/GHC/Domain.fs
+++ hashCode/GHC/Domain.fs
@@ -12,7 +12,8 @@ open GHC.Extensions.Common
 type Video = {idv : int ; size : int}
 
 type Point = {idP : int ; latency : int ; caches : int array}
-let createPoint id latency cacheNum = { idP = id ; latency = latency ; caches = Array.create cacheNum 0 } 
+
+let createPoint id latency cacheNum = { idP = id ; latency = latency ; caches = Array.create cacheNum -1 } 
 
 type Request = { video : int ; point : int ; value : int }
 
